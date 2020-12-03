8dea3cc14287e92229acdcfc38a89a533efa19b2
Hamed Jimoh <58299107+hmdNetizen@users.noreply.github.com>
2020-12-03 08:38:47 -0800

    Fixed the href link for the anchor tags

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -9,25 +9,25 @@ const Footer = (props) => {
       }`}
     >
       <ul className="footer__list">
-        <a href="#" className="footer__link">
+        <a href="/" className="footer__link">
           <li className="footer__list__item">
             <i className="fas fa-home list__item__icon"></i>
             <p className="list__item__text">Home</p>
           </li>
         </a>
-        <a href="#" className="footer__link">
+        <a href="/" className="footer__link">
           <li className="footer__list__item">
             <i className="fas fa-search list__item__icon"></i>
             <p className="list__item__text">Search</p>
           </li>
         </a>
-        <a href="#" className="footer__link">
+        <a href="/" className="footer__link">
           <li className="footer__list__item">
             <i className="far fa-bookmark list__item__icon"></i>
             <p className="list__item__text">Bookmarks</p>
           </li>
         </a>
-        <a href="#" className="footer__link">
+        <a href="/" className="footer__link">
           <li className="footer__list__item">
             <i className="fas fa-bars list__item__icon"></i>
             <p className="list__item__text">Menu</p>
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment } from "react";
 import SwitchButton from "../SwitchButton";
 import { connect } from "react-redux";
 
