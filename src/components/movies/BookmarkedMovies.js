86c973b508f58c6cff071f65cfcb6e36fd194f83
Hamed Jimoh <58299107+hmdNetizen@users.noreply.github.com>
2021-01-01 23:03:35 -0800

    Implemented dark mode for both viewed page and bookmarked page

--- src/components/movies/BookmarkedMovies.js
+++ src/components/movies/BookmarkedMovies.js
@@ -1,9 +1,20 @@
 import React from "react";
+import { connect } from "react-redux";
 
-const BookmarkedMovies = () => {
+const BookmarkedMovies = ({ darkMode }) => {
   return (
-    <div className="bookmarked">
-      <p>
+    <div
+      className={`bookmarked ${
+        darkMode ? "bookmarked--darkMode" : "bookmarked--lightMode"
+      }`}
+    >
+      <p
+        className={`bookmarked__defaultText ${
+          darkMode
+            ? "bookmarked__defaultText--darkMode"
+            : "bookmarked__defaultText--lightMode"
+        }`}
+      >
         You currently have no movie bookmarked. Click bookmark on the movie page
         to see it here.
       </p>
@@ -11,4 +22,10 @@ const BookmarkedMovies = () => {
   );
 };
 
-export default BookmarkedMovies;
+const mapStateToProps = (state) => {
+  return {
+    darkMode: state.theme.darkTheme,
+  };
+};
+
+export default connect(mapStateToProps)(BookmarkedMovies);
--- src/components/movies/ShowViewedMovies.js
+++ src/components/movies/ShowViewedMovies.js
@@ -2,13 +2,23 @@ import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { getSingleMovie } from "../../redux";
 
-const ShowViewedMovies = ({ viewed, match, getViewedMovies }) => {
+const ShowViewedMovies = ({ viewed, match, getViewedMovies, darkMode }) => {
   useEffect(() => {
-    getViewedMovies(match.id);
+    // getViewedMovies(match.id);
   }, [getViewedMovies, match]);
   return (
-    <div className="viewed">
-      <p>
+    <div
+      className={`viewed ${
+        darkMode ? "viewed--darkMode" : "viewed--lightMode"
+      }`}
+    >
+      <p
+        className={`viewed__defaultText ${
+          darkMode
+            ? "viewed__defaultText--darkMode"
+            : "viewed__defaultText--lightMode"
+        }`}
+      >
         You currently haven't viewed any movie. Click on any movie to keep track
         of the movies you have viewed
       </p>
@@ -19,6 +29,7 @@ const ShowViewedMovies = ({ viewed, match, getViewedMovies }) => {
 const mapStateToProps = (state) => {
   return {
     viewed: state.movies.viewedMovies,
+    darkMode: state.theme.darkTheme,
   };
 };
 const mapDispatchToProps = (dispatch) => {
